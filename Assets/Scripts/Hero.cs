a638f3aec75a2f0f32831bdadf12acc8ab5d4eff
DiHo3aBpik <aresnykorinets@yandex.ru>
2024-05-22 22:28:18 +0300

    new fon and level

--- Assets/Scripts/Hero.cs
+++ Assets/Scripts/Hero.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditorInternal;
 using UnityEngine;
 
 public class Hero : MonoBehaviour
@@ -11,13 +12,15 @@ public class Hero : MonoBehaviour
 
     private Rigidbody2D rb;
     private SpriteRenderer sprite;
+    private Animator anim;
 
 
     private void Awake()
     {
-        
+
         rb = GetComponent<Rigidbody2D>();
         sprite = GetComponentInChildren<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
     private void FixedUpdate()
@@ -27,18 +30,21 @@ public class Hero : MonoBehaviour
 
     private void Update()
     {
+        if (isGrounded) State = States.idle;
         if (Input.GetButton("Horizontal"))
             Run();
         if (isGrounded && Input.GetButtonDown("Jump"))
             Jump();
+        
+
     }
 
     private void Run()
     {
+        if (isGrounded) State= States.run;
+
         Vector3 dir = transform.right * Input.GetAxis("Horizontal");
-
         transform.position = Vector3.MoveTowards(transform.position, transform.position + dir, speed * Time.deltaTime);
-
         sprite.flipX = dir.x < 0.0f;
     }
 
@@ -51,5 +57,20 @@ public class Hero : MonoBehaviour
     {
         Collider2D[] collider = Physics2D.OverlapCircleAll(transform.position, 0.3f);
         isGrounded = collider.Length > 1;
+
+        if (!isGrounded ) State = States.jump;
     }
+
+    private States State
+    {
+        get { return (States)anim.GetInteger("state"); }
+        set { anim.SetInteger("state", (int)value); }
+    }
+}
+
+public enum States
+{
+    idle,
+    run,
+    jump
 }
